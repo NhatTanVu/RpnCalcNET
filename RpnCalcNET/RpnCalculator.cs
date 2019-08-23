dda07639d986b78aa00e6cbe878733209357b9e2
NhatTanVu <onchua2006@gmail.com>
2019-08-23 09:27:05 +0800

    Initial commit

---
+++ RpnCalcNET/RpnCalculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpnCalcNET
+{
+    public class RpnCalculator
+    {
+        public const char SEPARATOR = ' ';
+        public const string PLUS = "+";
+        public const string MINUS = "-";
+        public const string MULTIPLY = "*";
+        public const string DIVIDE = "/";
+        public readonly string[] OPERATORS = new string[] { PLUS, MINUS, MULTIPLY, DIVIDE };
+
+        public decimal Calculate(string expression)
+        {
+            if (string.IsNullOrEmpty(expression))
+                throw new ArgumentNullException();
+
+            string[] elements = expression.Split(SEPARATOR);
+            Stack<decimal> numberStack = new Stack<decimal>();
+            
+            for(int i = 0; i < elements.Length; i++)
+            {
+                string element = elements[i];
+                if (!OPERATORS.Contains(element))
+                {
+                    numberStack.Push(decimal.Parse(element));
+                }
+                else
+                {
+                    decimal secondNumber = numberStack.Pop();
+                    decimal firstNumber = numberStack.Pop();
+                    decimal answer = 0;
+
+                    switch (element) {
+                        case PLUS:
+                            answer = firstNumber + secondNumber;
+                            break;
+                        case MINUS:
+                            answer = firstNumber - secondNumber;
+                            break;
+                        case MULTIPLY:
+                            answer = firstNumber * secondNumber;
+                            break;
+                        case DIVIDE:
+                            answer = Decimal.Truncate(firstNumber / secondNumber);
+                            break;
+                    }
+
+                    numberStack.Push(answer);
+                }
+            }
+
+            decimal result = numberStack.Pop();
+            return result;
+        }
+    }
+}
---
+++ RpnCalcNET/RpnCalculatorTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpnCalcNET
+{
+    [TestFixture]
+    public class RpnCalculatorTests
+    {
+        [Test(Description = "Plus two numbers works")]
+        public void PlusTwoNumbersWorks()
+        {
+            RpnCalculator calculator = new RpnCalculator();
+            string expression = "1 2 +";
+            Assert.That(calculator.Calculate(expression) == 3);
+        }
+
+        [Test(Description = "Minus two numbers works")]
+        public void MinusTwoNumbersWorks()
+        {
+            RpnCalculator calculator = new RpnCalculator();
+            string expression = "4 5 -";
+            Assert.That(calculator.Calculate(expression) == -1);
+        }
+
+        [Test(Description = "Multiply two numbers works")]
+        public void MultiplyTwoNumbersWorks()
+        {
+            RpnCalculator calculator = new RpnCalculator();
+            string expression = "2 3 *";
+            Assert.That(calculator.Calculate(expression) == 6);
+        }
+
+        [Test(Description = "Divide two numbers works")]
+        public void DivideTwoNumbersThenTruncated()
+        {
+            RpnCalculator calculator = new RpnCalculator();
+            string expression = "4 3 /";
+            Assert.That(calculator.Calculate(expression) == Decimal.Truncate(4/3));
+        }
+
+        [Test(Description = "Complex operation works")]
+        public void ComplexOperationWorks()
+        {
+            RpnCalculator calculator = new RpnCalculator();
+            string expression = "15 7 1 1 + - / 3 * 2 1 1 + + -";
+            Assert.That(calculator.Calculate(expression) == 5);
+        }
+
+        [Test(Description = "Null expression then throws exception")]
+        public void NullExpressionThenThrowException()
+        {
+            RpnCalculator calculator = new RpnCalculator();
+            string expression = null;
+            Assert.Throws(typeof(ArgumentNullException), delegate { calculator.Calculate(expression); });
+        }
+
+        [Test(Description = "Empty expression then throws exception")]
+        public void EmptyExpressionThenThrowException()
+        {
+            RpnCalculator calculator = new RpnCalculator();
+            string expression = "";
+            Assert.Throws(typeof(ArgumentNullException), delegate { calculator.Calculate(expression); });
+        }
+    }
+}
